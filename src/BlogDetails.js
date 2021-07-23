f7503d73d0fa04eec7b7b49ed9fe8cdf85fc25fb
Shaun Robinson <Sarobinson426@gmail.com>
2021-07-22 19:42:54 -0700

    Added the code to the BlogDetails page by retreiving blog from db.json using useFetch and parameters in the router. Modified some css to get rid of underline on the links from home page.

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,23 @@
 import { useParams } from 'react-router-dom';
+import useFetch from './useFetch.js';
 
 const BlogDetails = () => {
     const { id } = useParams();
+    const { data, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
 
     return(
         <div className="content">
-            <h2>Blog Details - { id }</h2>
+            { isPending && <div>Pending...</div> }
+            { error && <div>{ error }</div> }
+            { data && (
+               <div className="blog-details">
+                   <article>
+                       <h1>{ data.title }</h1>
+                       <h2>Authored by: { data.author }</h2>
+                       <p>{ data.body }</p>
+                   </article>
+               </div>
+            )}
         </div>
     );
 }
